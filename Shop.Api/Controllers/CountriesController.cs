a7b6eb73d201575ffead7201096a5be6218ca461
B166er-1994 <ramosfeliberto695@gmail.com>
2023-04-05 04:05:37 +0200

    Added Get and Post method in the controllerCountries Api Controller

---
+++ Shop.Api/Controllers/CountriesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.API.Data;
+using Shop.Shared.Entities;
+
+namespace Shop.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context) //inyeccion de dependencia del contexto de datos
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        { 
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
--- Shop.Api/Program.cs
+++ Shop.Api/Program.cs
@@ -26,4 +26,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//////////Configurando la seguridad del sistema
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.Run();
