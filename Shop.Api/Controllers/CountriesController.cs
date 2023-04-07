e88cc486c82f754cf3d68c1417054be8f37c9ed9
B166er-1994 <ramosfeliberto695@gmail.com>
2023-04-07 21:22:04 +0200

    Added CRUD methods to /countries controller

--- Shop.Api/Controllers/CountriesController.cs
+++ Shop.Api/Controllers/CountriesController.cs
@@ -27,5 +27,41 @@ namespace Shop.Api.Controllers
             await _context.SaveChangesAsync();
             return Ok(country);
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var afectedRows = await _context.Countries
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (afectedRows == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
     }
 }
