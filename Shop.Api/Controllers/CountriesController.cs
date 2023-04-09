0a540f1488ab943bf6b4ad799be90f54ecf5b4de
B166er-1994 <ramosfeliberto695@gmail.com>
2023-04-09 22:33:20 +0200

    Done Multilevel Crud Countries

--- Shop.Api/Controllers/CountriesController.cs
+++ Shop.Api/Controllers/CountriesController.cs
@@ -53,7 +53,10 @@ namespace Shop.Api.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult> GetAsync(int id)
         {
-            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            var country = await _context.Countries
+                .Include (x => x.States!)
+                .ThenInclude(x => x.Cities)
+                .FirstOrDefaultAsync(x => x.Id == id);
             if (country is null)
             {
                 return NotFound();
