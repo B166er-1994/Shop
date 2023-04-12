93b6caa9bf0e571fd41a90498ce0c7e8f1eb535b
B166er-1994 <ramosfeliberto695@gmail.com>
2023-04-12 16:44:50 +0200

    DTO Filter  for searching countries

--- Shop.Api/Controllers/CountriesController.cs
+++ Shop.Api/Controllers/CountriesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shop.API.Data;
+using Shop.API.Helpers;
+using Shop.Shared.DTOs;
 using Shop.Shared.Entities;
 
 namespace Shop.Api.Controllers
@@ -16,12 +18,39 @@ namespace Shop.Api.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<IActionResult> GetAsync()
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)//Added pagination
         {
-            return Ok(await _context.Countries
-                .Include(x => x.States)
+            var queryable = _context.Countries
+                .Include(x => x.States).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+
+            return Ok(await queryable
+                .OrderBy(x => x.Name)
+                .Paginate(pagination)
                 .ToListAsync());
         }
+
+        [HttpGet("totalPages")]//Counting pages
+        public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Countries.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
+        }
+
         [HttpPost]
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
